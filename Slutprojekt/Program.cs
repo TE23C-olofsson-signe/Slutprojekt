3dba6bc719c03a30f94a4ddf8e803225ebba0d33
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2025-03-14 14:31:49 +0100

    börjat med slutprojektek

---
+++ Slutprojekt/Program.cs
@@ -0,0 +1,27 @@
+﻿// Välja vilken karaktär 
+bool done = false; 
+string charactername=""; 
+
+List<string>character =["Red","Chloe"];
+
+foreach (var item in character)
+{
+    Console.WriteLine(item); 
+}
+
+static void Prompt()
+{
+    Console.WriteLine(">"); 
+}
+
+static void fightning(string weapon,string characterName,string bruteName,int weaponHp,int bruteHp)
+{
+    Console.WriteLine($"{characterName} attackerar{bruteName} med {weapon}"); 
+
+    while(bruteHp>0)
+    {
+        Console.WriteLine($"\n-----==== Slå {bruteName}====----");
+        
+    }
+}
+Console.ReadLine(); 
