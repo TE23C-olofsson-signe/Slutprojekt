e10c90f429d546d46a5e6a5fe4a6ebe6c358f773
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2025-03-23 14:59:25 +0100

    välja vapen och skurkar

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -12,8 +12,8 @@ foreach (var item in character)
 Prompt();
 
 while (!done)
-charactername= Console.ReadLine(); 
 {
+    charactername= Console.ReadLine(); 
     if (character.Contains(charactername))
     {
         Console.WriteLine($"nu är du {charactername}"); 
@@ -25,15 +25,67 @@ charactername= Console.ReadLine();
     }
 }
 
+if (charactername=="Red")
+{
+    Red(); 
+}
+
 static void Red()
 {
+    // Spawn punkt 
     Console.WriteLine("Du spawnar vid Merlin Academy"); 
-    Console.WriteLine("Du börjar gå tills du möter Bridget hon säger att du måste välja mellan 3 vapen att köpa du har 120kr");
-    
-    int kr=120;
-    int cost=0; 
+    Console.WriteLine("Du börjar gå tills du möter Bridget hon säger att du måste välja mellan 3 vapen");
 
-    Console.WriteLine("Svärd (10kr)"); 
+    List<string> vapen= ["Svärd","Pilbåge och pilar","Formelbok"];
+
+    foreach (var item in vapen)
+    {
+        Console.WriteLine(item); 
+    }
+
+    Prompt(); 
+
+    string vapenname=""; 
+    bool flamingo= false; 
+
+    while (!flamingo)
+    {
+        vapenname=Console.ReadLine(); 
+        if (vapen.Contains(vapenname))
+        {
+            Console.WriteLine($"Du valde {vapenname}"); 
+            flamingo= true; 
+        }
+    }
+
+    // Vilken skurk man slåss mot
+    Console.WriteLine("Beronde på vilket vapen du valde är det en annan skurk"); 
+    
+    int skurkHp = 600;
+    string skurkname=""; 
+    int YourvapenHp=0;  
+    
+
+    if (vapenname=="Svärd")
+    {
+        YourvapenHp=40; 
+        skurkname="Queen of hearts"; 
+    }
+    else if (vapenname=="Pilbåge och pilar")
+    {
+        YourvapenHp=70; 
+        skurkname="Hook"; 
+    }
+    else if (vapenname=="Formelbok")
+    {
+        YourvapenHp=90;
+        skurkname="Uliana"; 
+    }
+
+    fightning(vapenname,"Red",skurkname,YourvapenHp,skurkHp);
+
+
+
 }
 
 static void Chloe()
@@ -43,8 +95,13 @@ static void Chloe()
 
 static void Prompt()
 {
-    Console.WriteLine(">");
+    Console.Write("> ");
 }
+
+static void uppgradesRed()
+{
+    int 
+} 
 
 static void fightning(string weapon, string characterName, string bruteName, int weaponHp, int bruteHp)
 {
@@ -61,6 +118,7 @@ static void fightning(string weapon, string characterName, string bruteName, int
         Console.WriteLine($"{weapon} gör{weaponDamage} på {bruteName}");
         Console.ReadKey();
     }
+    Console.WriteLine($"Yay du slog {bruteName}");
 }
 
 static void rescuefight(string weapon, string characterName, string recsuename, string bruteName, int weaponHp2, int bruteHp2)
@@ -78,6 +136,7 @@ static void rescuefight(string weapon, string characterName, string recsuename, 
         Console.WriteLine($"{weapon} gör{weaponDamage} på {bruteName}");
         Console.ReadKey();
     }
+    Console.WriteLine($"Yay du lyckades rädda  {recsuename} och du lyckades med äventyret");
 
 }
 Console.ReadLine();
